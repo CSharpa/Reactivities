0369693eda2223d93aec31024a2c4d4cfacdaf5c
Jarrod Clarke <jarrodmc@outlook.com>
2020-12-15 07:51:39 +0200

    End of Section 3

---
+++ client-app/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios'
+import { Header, Icon, List } from 'semantic-ui-react'
+
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+    .then((response) => {
+      this.setState({
+        values: response.data
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as='h2' icon textAlign='center'>
+          <Icon name='users' circular />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value) => (
+                <List.Item key={value.id}>{value.name}</List.Item>
+              ))}
+        </List>
+      </div>
+    )  
+  }
+}
+
+export default App;
